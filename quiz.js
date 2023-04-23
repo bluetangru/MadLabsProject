7cda7ca4aeab27566f2bc88ffeedd40877e337c7
bluetangru <131351945+bluetangru@users.noreply.github.com>
2023-04-23 16:36:41 +0100

    finalising quiz.js

--- quiz.js
+++ quiz.js
@@ -7,17 +7,20 @@ function checkAnswers() {
     //assign the quiz elements to the quiz variable
     quiz=document.forms.Quiz.elements;
     //assign the answer to the question to the answer1 variable
-    answer1= quiz.growth.value;
+    answer1= quiz.q1.value;
     //check the answer to the first question
-    if (answer1=="70%") {
+    if (answer1=="yellow%") {
         score = score+1;
     }
-    answer2 = quiz.talent.value;
-    if (answer2=="50") {
+    answer2 = quiz.q2.value;
+    if (answer2=="chester" || answer2=="university") {
         score = score+1;
     }
+	answer3 = quiz.q3.value;
+	if (answer3=="25") {
+		score = score+1;
 userName = quiz.name.value;
-alert ("Well done " + userName + " your score was..." + score);
+alert ("Well done " + userName + "," + " your score is " + score);
 }
 
 /*DOM =document object model
